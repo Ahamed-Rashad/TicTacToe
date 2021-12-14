f8a97f58c20978d70a1119b7ff8b02d83ad94023
Ahamed Rashad <77972523+Ahamed-Rashad@users.noreply.github.com>
2021-12-14 23:27:56 +0530

    Tac Tic Toe

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css';
+import Tiktacktoe, { Game } from "./component/Tiktacktoe";
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Tiktacktoe/>
+      <Game/>
+
+    </div>
+  );
+}
+
+export default App;
